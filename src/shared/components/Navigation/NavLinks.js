3968e3bf8e63237fabd6a1502d1cbe339415b07c
Marco Ramirez <marco.ramirez@ugto.mx>
2025-06-11 15:35:16 -0600

    [ADDED]: add menu and toggle btn

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import "./MainNavigation.css";
+import "./NavLinks.css";
 
 const NavLinks = props => {
-  return ()
+  return <ul className="nav-links">
+    <li>
+      <NavLink to="/">All Users</NavLink>
+    </li>
+    <li>
+      <NavLink to="/u1/places">My Places</NavLink>
+    </li>
+     <li>
+      <NavLink to="/places/new">Add Place</NavLink>
+    </li>
+     <li>
+      <NavLink to="/auth">Authenticate</NavLink>
+    </li>
+  </ul>
 }
 
 export default NavLinks;
